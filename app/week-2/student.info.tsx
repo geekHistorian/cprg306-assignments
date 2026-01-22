bf1fab6707f9e47db8b6a480cba0544812441091
geekHistorian <rishabhraturi299@gmail.com>
2026-01-22 14:05:20 -0700

    final push for assignment 2

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function StudentInfo() {
+    return (
+        <div>
+            <h1>My name: Rishabh Raturi</h1>
+            <h1><a href="https://github.com/geekHistorian/cprg306-assignments.git">GitHub Repository</a></h1>
+        </div>
+    )
+
+};
+
+export default StudentInfo;
